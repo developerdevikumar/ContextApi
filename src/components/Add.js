a52e3529f7fc22d5d1614090516ca4024633a7c0
kumar indala <91934@DESKTOP-VV8OPAL.micelio.local>
2021-04-08 15:07:50 +0530

    api

--- src/components/Add.js
+++ src/components/Add.js
@@ -11,7 +11,7 @@ export const Add = () => {
     setQuery(e.target.value);
 
     fetch(
-      `https://api.themoviedb.org/3/movie/550?api_key=a2505c705b7b750d62d8b94876846868&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+      ` https://api.themoviedb.org/3/search/movie?api_key=a2505c705b7b750d62d8b94876846868&language=en-US&page=1&include_adult=false&query=${e.target.value}`
     )
       .then((res) => res.json())
       .then((data) => {
